fefe9e0d8a1acfbdd475b1aa4934cc1e93c857f1
stevenB88-svg <barreto.900@icloud.com>
2025-02-12 13:27:08 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3000; // Usa el puerto 3000 si no se especifica otro
+
 const app = express();
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.send("Hello World 2");
 });
 
-app.listen(80);
+// Iniciar el servidor y manejar posibles errores
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+}).on('error', (err) => {
+    console.error(`Error al iniciar el servidor: ${err.message}`);
+});
